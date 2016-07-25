a9ffdf36880ba3cbb08a2d29b7ccf80c1d944050
kit jonathan ho <j.hokit80@gmail.com>
2016-07-25 20:48:30 +0800

    static pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725073228) do
+ActiveRecord::Schema.define(version: 20160725084000) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "content"
+    t.string   "email"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
